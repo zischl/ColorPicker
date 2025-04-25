0eed499eed701e6b066a4ce37df438ee6834b643
zischl <147017426+zischl@users.noreply.github.com>
2025-04-25 14:35:50 +0530

    fixed timeIT not taking kwargs

--- TimeIt.py
+++ TimeIt.py
@@ -1,8 +1,8 @@
 import time
 def TimeIT(func):
-        def inner(*args): 
+        def inner(*args, **kwargs): 
             time1= time.perf_counter()
-            bleh = func(*args)
+            bleh = func(*args, **kwargs)
             time2= time.perf_counter()
             print("Time Taken : ",time2-time1)
             return bleh
