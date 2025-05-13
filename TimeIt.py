b31c6c4620bd7e78ab86911b96d26b2f197d20a8
zischl <147017426+zischl@users.noreply.github.com>
2025-05-13 17:03:24 +0530

    * Fixed bug where chronodict wasn't accessible

--- TimeIt.py
+++ TimeIt.py
@@ -21,6 +21,17 @@ def TimeITAvg(id):
         return inner
     return Outer
     
+def TimeItAuto(func):
+    id = func.__name__
+    if id not in Chronos.chronodict: Chronos.chronodict[id] = [] 
+    def inner(*args, **kwargs): 
+        time1= time.perf_counter()
+        bleh = func(*args, **kwargs)
+        time2= time.perf_counter()
+        Chronos.chronodict[id].append(time2-time1)
+        return bleh
+    return inner
+
 class Chronos():
     chronodict = {}
     def __init__(self):
@@ -28,13 +39,21 @@ class Chronos():
     
     @classmethod
     def PerfIT(self):
-        self.avgTime(*Chronos.chronodict.values())
+        for chronofunc, chronolist in Chronos.chronodict.items():
+            if not chronolist: 
+                print(f"{chronofunc} ran 0 times")
+                continue
+            print(f"Time Taken for function {chronofunc} : Average - {sum(chronolist)/len(chronolist)}, Max - {max(chronolist)}, Min - {min(chronolist)}, ran {len(chronolist)} times")
 
+    @classmethod
     def avgTime(*args):
         
         limit = min(len(chronolist) for chronolist in Chronos.chronodict.values())
         
         for chronofunc, chronolist in Chronos.chronodict.items():
+            print(chronofunc)
             print(f"Average Time Taken for function {chronofunc} :",sum(chronolist[:limit])/len(chronolist[:limit]))
             print(f"Min Time Taken for function {chronofunc} :",min(chronolist[:limit]))
             print(f"Max Time Taken for function {chronofunc} :",max(chronolist[:limit]))
+            
+            
